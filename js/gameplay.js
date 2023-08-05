e29301ecda4479481e4f49f42049bc12c5a29b65
samphillips123 <sphillips123@gmail.com>
2023-08-05 13:49:31 -0700

    added Dice class, randomDie number generator funtion, and tested

--- js/gameplay.js
+++ js/gameplay.js
@@ -1,13 +1,20 @@
 console.log('welcome to gameplay')
+// Global funcitons
+const randomDie = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
 
-// Set up objects and classes for players, gamepieces, and board game spaces.
+console.log(randomDie(1,6))
+
+// Set up objects and classes for players, gamepieces, dice, and board game spaces.
 // Player class
 class Player {
-    constructor(name, gamePiece, bank, ownedProperties) {
+    constructor(name, gamePiece, bank, ownedProperties, currentSpace) {
         this.name = name;
         this.gamePiece = gamePiece;
         this.bank = 1000;
         this.ownedProperties = [];
+        this.currentSpace = 0;
     }
 
 }
@@ -28,3 +35,14 @@ class GameSpace {
 // create test GameSpace
 const gameSpaceTest = new GameSpace('property', true, 1)
 console.log(gameSpaceTest)
+
+// Dice class
+class Dice {
+    constructor(number) {
+        this.number = randomDie(1, 6)
+    }
+}
+
+// create die
+const Dice1 = new Dice()
+console.log(Dice1)
