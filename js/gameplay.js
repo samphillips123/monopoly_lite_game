0fb801f64e72ff08260baf42f4adb37329c9474a
samphillips123 <sphillips123@gmail.com>
2023-08-05 14:22:20 -0700

    added class extension for Railroad types and added name to GameSpace class

--- js/gameplay.js
+++ js/gameplay.js
@@ -22,7 +22,8 @@ console.log(playerTest)
 
 // GameSpace class
 class GameSpace {
-    constructor(spaceType, spaceNum, ownable) {
+    constructor(name, spaceType, spaceNum, ownable) {
+        this.name = name;
         this.spaceType = spaceType;
         this.ownable = ownable;
         this.spaceNum = spaceNum;
@@ -34,10 +35,10 @@ class GameSpace {
 // const gameSpaceTest = new GameSpace('property', true, 1)
 // console.log(gameSpaceTest)
 
-// extention for GameSpace -- Property
+// extension for GameSpace -- Property
 class Property extends GameSpace {
-    constructor(spaceNum, groupColor, cost, rent, rentAll, spaceType, numInGroup, ownable) {
-        super(spaceType, spaceNum, ownable)
+    constructor(name, spaceNum, groupColor, cost, rent, rentAll, spaceType, numInGroup, ownable) {
+        super(name, spaceType, spaceNum, ownable)
         this.groupColor = groupColor;
         this.cost = cost;
         this.rent = rent;
@@ -48,8 +49,28 @@ class Property extends GameSpace {
     }
 }
 // create test property
-const salinas = new Property(1, 'blue', 35, 15)
+const salinas = new Property('salinas', 1, 'blue', 35, 15)
 console.log(salinas)
+
+// extension for GameSpace -- Railroad
+class Railroad extends GameSpace {
+    constructor(name, spaceNum, groupColor, cost, rent1, rent2, rent3, rent4, spaceType, numInGroup, ownable) {
+        super(name, spaceType, spaceNum, ownable)
+        this.name = name + ' Railroad';
+        this.groupColor = 'white';
+        this.cost = 100;
+        this.rent1 = 20;
+        this.rent2 = 40;
+        this.rent3 = 80;
+        this.rent4 = 160;
+        this.spaceType = 'Railroad'
+        this.numInGroup = 4;
+        this.ownable = true;
+    }
+}
+// create test railroad
+const marina = new Railroad('Marina', 3)
+console.log(marina)
 
 // Dice class
 class Dice {
