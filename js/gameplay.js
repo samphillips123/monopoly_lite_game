e5370b4fda6a4d29d1980d314e6d16b8fe02b9e2
samphillips123 <sphillips123@gmail.com>
2023-08-05 11:53:39 -0700

    set up properties for player class

--- js/gameplay.js
+++ js/gameplay.js
@@ -1,1 +1,13 @@
 console.log('welcome to gameplay')
+
+// Set up objects and classes for players, gamepieces, and board game spaces.
+// Player class
+class Player {
+    constructor(name, gamePiece, bank, ownedProperties) {
+        this.name = '';
+        this.gamePiece '';
+        this.bank = 1000;
+        this.ownedProperties = [];
+    }
+    
+}
