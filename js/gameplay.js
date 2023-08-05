31a023dc11238af71aec83ab075303985bdc39e6
samphillips123 <sphillips123@gmail.com>
2023-08-05 16:31:48 -0700

    update spaceGen function to use specific classes for generation depenting on location

--- js/gameplay.js
+++ js/gameplay.js
@@ -229,8 +229,27 @@ console.log(spaces)
 // For loop to create all space names
 const spaceGen = (numSpaces) => {
     for (let i = 0; i < numSpaces; i++) {
-        spaces[i] = new Property(properties[i].name, i, properties[i].groupColor, properties[i].cost, properties[i].rent)
+        // check if i equals the corner space # by splitting the numSpaces into 4
+        if (i === 0 || i === numSpaces * 0.25 || i === numSpaces * 0.5 || i === numSpaces * 0.75) {
+            // loop through corners array with j but specifying spaceNum with i
+            for (let j = 0; j < corners.length; j++) {
+                spaces[i] = new Corner(corners[j].name, i)
+            }
+        // check if i equals the middle spaces by checking 8ths (1/8, 3/8, 5/8, 7/8)
+        } else if (i === numSpaces * 0.125 || i === numSpaces * 0.375 || i === numSpaces * 0.625 || i === numSpaces * 0.875) {
+            // loop through railroads array with k but specifying spaceNum with i
+            for (let k = 0; k < railroads.length; k++) {
+                spaces[i] = new Railroad(railroads[k].name, i)
+            }
+        } else {
+            // all other spaces use the properties array.
+            for (let l = 0; l < properties.length; l++) {
+                // loop through properties array with l but specifying spaceNum with i
+                spaces[i] = new Property(properties[l].name, i, properties[l].groupColor, properties[l].cost, properties[l].rent)
+            }
+        }
     }
 }
-spaceGen(properties.length)
+
+spaceGen(24)
 console.log(spaces)
