4a5012aac43583da26b7c184769f76c048fabbf0
samphillips123 <sphillips123@gmail.com>
2023-08-05 14:10:30 -0700

    added class extention for property types of spaces

--- js/gameplay.js
+++ js/gameplay.js
@@ -3,8 +3,7 @@ console.log('welcome to gameplay')
 const randomDie = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1) + min)
 }
-
-console.log(randomDie(1,6))
+// console.log(randomDie(1,6))
 
 // Set up objects and classes for players, gamepieces, dice, and board game spaces.
 // Player class
@@ -16,33 +15,48 @@ class Player {
         this.ownedProperties = [];
         this.currentSpace = 0;
     }
-
 }
-
 // create test player
 const playerTest = new Player('Sam', 'tophat')
 console.log(playerTest)
 
 // GameSpace class
 class GameSpace {
-    constructor(spaceType, ownable, spaceNum) {
+    constructor(spaceType, spaceNum, ownable) {
         this.spaceType = spaceType;
-        this.type = ownable;
+        this.ownable = ownable;
         this.spaceNum = spaceNum;
+        
+
     }
 }
-
 // create test GameSpace
-const gameSpaceTest = new GameSpace('property', true, 1)
-console.log(gameSpaceTest)
+// const gameSpaceTest = new GameSpace('property', true, 1)
+// console.log(gameSpaceTest)
+
+// extention for GameSpace -- Property
+class Property extends GameSpace {
+    constructor(spaceNum, groupColor, cost, rent, rentAll, spaceType, numInGroup, ownable) {
+        super(spaceType, spaceNum, ownable)
+        this.groupColor = groupColor;
+        this.cost = cost;
+        this.rent = rent;
+        this.rentAll = Math.ceil(this.rent * 1.25);
+        this.spaceType = 'Property'
+        this.numInGroup = 2;
+        this.ownable = true;
+    }
+}
+// create test property
+const salinas = new Property(1, 'blue', 35, 15)
+console.log(salinas)
 
 // Dice class
 class Dice {
     constructor(number) {
-        this.number = randomDie(1, 6)
+        this.number = randomDie(1, 6) // standard 6 sided die
     }
 }
-
 // create die
 const Dice1 = new Dice()
 console.log(Dice1)
